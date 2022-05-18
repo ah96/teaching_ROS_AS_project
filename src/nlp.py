24e9f81bec3e8e5930a43dbc6c676df84478bbba
ah96 <amarhalilovic96@gmail.com>
2022-05-18 15:02:21 +0200

    .

--- src/nlp.py
+++ src/nlp.py
@@ -6,11 +6,12 @@ import rospy
 # Brings in the SimpleActionClient
 import actionlib
 
-import sys
-
 import pal_interaction_msgs.msg
 
-def tts_client():
+from std_msgs.msg import String
+
+
+def tts_client(text_msg):
     # Creates the SimpleActionClient, passing the type of the action to the constructotr
     client = actionlib.SimpleActionClient('tts', pal_interaction_msgs.msg.TtsAction)
 
@@ -24,10 +25,13 @@ def tts_client():
     #goal.rawtext.text = 'Ich bin Tiago and ich kann sprechen. Wo ho. Hu ra.'
     #goal.rawtext.text = 'Ich heiße Tiago and ich kann schweizer Deutsch sprechen. Wo ho. Hu ra.'
     #goal.rawtext.text = 'Das passiert, wenn ein Amerikaner versucht, Deutsch zu sprechen.'
-    goal.rawtext.text = 'Uncle Sam wants you for the US army.'
+    #goal.rawtext.text = 'Uncle Sam wants you for the US army.'
     #goal.rawtext.text = 'It seems that I cannot speak German. Why am I then in Germany?'
     #goal.rawtext.text = 'How can I get out from this lab?'
-    goal.rawtext.lang_id = 'en_US'
+    
+    goal.rawtext.text = str(text_msg)
+    goal.rawtext.lang_id = 'de_DE' #'en_US' or 'de_DE'
+    print(text_msg)
 
     # Sends the goal to the action server.
     client.send_goal(goal)
@@ -38,12 +42,22 @@ def tts_client():
     # Prints out the result of executing the action
     return client.get_result()  # A FibonacciResult
 
+def text_exps_callback(msg):
+    result = tts_client(msg.data)
+
 if __name__ == '__main__':
     try:
         # Initializes a rospy node so that the SimpleActionClient can
         # publish and subscribe over ROS.
         rospy.init_node('tts_client_py')
-        result = tts_client()
-        #print("Result:", ', '.join([str(n) for n in result.sequence]))
+
+        #result = tts_client('I am Tiago!')
+
+        # Initalize a subscriber to the "/camera/rgb/image_raw" topic with the function "image_callback" as a callback
+        sub_text_expls = rospy.Subscriber("/text_exps", String, text_exps_callback)
+        # Loop to keep the program from shutting down unless ROS is shut down, or CTRL+C is pressed
+        while not rospy.is_shutdown():
+            rospy.spin()
+    
     except rospy.ROSInterruptException:
         print("program interrupted before completion")
